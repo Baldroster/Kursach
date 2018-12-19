ca2239dc17e722bc557ff61d6182dedccd955175
Anthropologiae <46005095+Anthropologiae@users.noreply.github.com>
2018-12-19 21:47:05 +0300

    fgfdg
    соььше

--- src/ExecutionCode/Main.java
+++ src/ExecutionCode/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
@@ -19,5 +20,16 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+
+
+
+
+    public static void printserver () {
+
+        System.out.print ("Запуш меня в Git");
+
+
     }
 }
